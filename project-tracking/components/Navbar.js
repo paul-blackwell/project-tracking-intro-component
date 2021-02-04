76c0b6319ecc0f82d6a1392267f7dbf8bfd6bce6
Paul Blackwell <blackwellpaul34@gmail.com>
2021-02-04 17:05:01 +0000

    Finished Navbar

--- project-tracking/components/Navbar.js
+++ project-tracking/components/Navbar.js
@@ -15,21 +15,21 @@ const Navbar = (props) => {
     }
 
     return (
-        <nav className="py-9">
+        <nav className="py-9 md:flex md:justify-between md:align-middle">
             <div className="flex justify-between align-middle">
                 <a href="#">
                     <img src="/images/logo.svg" alt="logo" />
                 </a>
-                <button onClick={toggle}>
+                <button onClick={toggle} className="md:hidden">
                     {navIcon}
                 </button>
             </div>
-            <ul className="flex flex-col mx-auto shadow-xl rounded-md font-bold text-2xl  text-neutral-400 uppercase" >
-                <li><a href="#" className="pt-3 text-center">Product</a></li>
-                <li><a href="#" className="pt-3 text-center">Features</a></li>
-                <li><a href="#" className="pt-3 text-center">Pricing</a></li>
+            <ul className={`${navOpen ? 'flex' : 'hidden'} flex-col md:flex md:flex-row border md:border-none  shadow-xl md:shadow-none rounded-md font-bold text-2xl  text-neutral-400 uppercase px-8 md:px-0 mt-10 md:mt-0`} >
+                <li><a href="#" className="py-5 md:py-0 md:mr-7 text-center block md:inline">Product</a></li>
+                <li><a href="#" className="pb-5 md:py-0 md:mr-7 text-center block md:inline">Features</a></li>
+                <li><a href="#" className="pb-5 md:py-0 md:mr-7 text-center block md:inline">Pricing</a></li>
                 <li><div></div></li>
-                <li><a href="#" className="py-3 text-center border-t-2 border-neutral-100 text-neutral-200">Login</a></li>
+                <li><a href="#" className="py-5 md:py-0  text-center border-t-2 border-neutral-100 md:border-none text-neutral-200 block">Login</a></li>
             </ul>
         </nav>
     )
