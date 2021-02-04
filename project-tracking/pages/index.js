45e9f0c4bce2fcc1fa9e19f81e03518714b12274
Paul Blackwell <blackwellpaul34@gmail.com>
2021-02-04 11:16:45 +0000

    Project setup

--- project-tracking/pages/index.js
+++ project-tracking/pages/index.js
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hi im a Next.js app</h1>
-    </div>
+      <div>
+        <h1 className="flex justify-center border-r-4 border-white text-primary font-body">Hi im a Next.js app</h1>
+      </div>
   )
 }
--- project-tracking/tailwind.config.js
+++ project-tracking/tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'hsl(0, 100%, 68%)',
+        neutral: {
+          100: 'hsl(207, 33%, 95%)',
+          200: 'hsl(231, 7%, 65%)',
+          300: ' hsl(230, 11%, 40%)',
+          400: 'hsl(230, 29%, 20%)'
+        },
+      },
+      fontFamily: {
+        body: ['Barlow Condensed']
+      }
+    },
   },
   variants: {
     extend: {},
