2242fd1a226ee5d2090e634984ef96a08300dcfd
Paul Blackwell <blackwellpaul34@gmail.com>
2021-02-05 11:18:24 +0000

    Was working on the Main section

--- project-tracking/components/Main.js
+++ project-tracking/components/Main.js
@@ -4,16 +4,16 @@ import SpanRounded from './SpanRounded';
 const Main = (props) => {
     return (
         <main>
-            <img src="/images/illustration-devices.svg" />
+            <img className="w-full h-auto" src="/images/illustration-devices.svg" />
             <header>
                 <SpanRounded>New</SpanRounded>
-                <span>Monograph dashboard</span>
-                <h1>
-                    <span>Powerful insights into your team</span>
+                <span className="text-neutral-200 tracking-wider uppercase">Monograph dashboard</span>
+                <h1 className="uppercase text-2xl font-bold">
+                    <span className="text-6xl ">Powerful insights into your team</span>
                 </h1>
-                <p>Project planning and time tracking</p>
-                <a href=""></a>
-                <a href=""></a>
+                <p className="text-neutral-300">Project planning and time tracking for agile teams</p>
+                <a href="#"></a>
+                <a href="#"></a>
             </header>
         </main>
     )
--- project-tracking/components/SpanRounded.js
+++ project-tracking/components/SpanRounded.js
@@ -1,6 +1,6 @@
 
 const SpanRounded = (props) => (
-    <span className="text-white bg-neutral-400 capitalize rounded-full py-0.5 px-4 mr-4">
+    <span className="text-white bg-neutral-400 uppercase rounded-full py-0.5 px-4 mr-4 font-bold tracking-wider">
         {props.children}
     </span>
 );
